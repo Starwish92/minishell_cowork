5b64623bf0cc4e95a69b3389027c8851c7bbf17f
Youngjin Park <youngjpa@cx1r5s1.42seoul.kr>
2023-04-07 15:35:12 +0900

    pyj_add1

--- parse/quotes_setting.c
+++ parse/quotes_setting.c
@@ -6,8 +6,36 @@
 /*   By: youngjpa <youngjpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 13:44:43 by youngjpa          #+#    #+#             */
-/*   Updated: 2023/04/07 13:44:54 by youngjpa         ###   ########.fr       */
+/*   Updated: 2023/04/07 15:17:12 by youngjpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-parse_set_quotes
+#include "minishell.h"
+
+int	parse_set_quotes(char line, int quotes, t_cmd_info *cmd) //리턴값 quote, 0으로 초기화된 상태
+{
+	int	result;
+
+	result = quotes;
+	if (line == '$')
+		cmd->ft_dollar_flag = true;
+	if (line == '\'') // ' 처리
+	{
+		if (quotes == 1) //2. '가 닫혔다면 다시 result = 0
+			result = 0;
+		else if (quotes == 2) //닫을게 있을때까지 돔.
+			result = 2;
+		else //1. '로 들어오면 result = 1
+			result = 1;
+	}
+	else if (line == '\"') // " 처리
+	{
+		if (quotes == 2) //2. "가 닫혔다면 다시 result를 0으로
+			result = 0;
+		else if (quotes == 1) //닫을게 있을때까지 돔.
+			result = 1;
+		else //1. "가 있고 quote가 0이라면 result == 2
+			result = 2;
+	}
+	return (result);
+}
